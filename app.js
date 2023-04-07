ab8ca8f35bf30ece361b9b904df6c3397d930bb2
stanken_a <artyemst3@mail.ru>
2023-04-07 16:41:17 +0300

    feat: add mongoose, add models for user and card

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/mestodb', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false
+});
 
 const { PORT = 3000 } = process.env;
 
