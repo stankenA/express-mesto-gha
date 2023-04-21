18fcfb89ab64ed31af7eb617d1679a0bba497c03
stanken_a <artyemst3@mail.ru>
2023-04-21 11:51:44 +0300

    feat: add email and password fields for user schema

--- app.js
+++ app.js
@@ -6,8 +6,8 @@ const routes = require('./routes/routes');
 const { PORT = 3000 } = process.env;
 const app = express();
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json()); // для собирания JSON-формата
+app.use(bodyParser.urlencoded({ extended: true })); // для приёма веб-страниц внутри POST-запроса
 
 mongoose.connect('mongodb://localhost:27017/mestodb');
 
