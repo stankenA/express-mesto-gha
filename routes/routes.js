36b7733c7a6b7266277d704ad39ad8d7e9321222
stanken_a <artyemst3@mail.ru>
2023-04-22 17:56:48 +0300

    fix: fix signup Joi validation

--- routes/routes.js
+++ routes/routes.js
@@ -18,8 +18,8 @@ router.post('/signin', celebrate({
 }), login);
 router.post('/signup', celebrate({
   body: Joi.object().keys({
-    name: Joi.string().required().min(2).max(30),
-    about: Joi.string().required().min(2).max(30),
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
     avatar: Joi.string().regex(new RegExp(regexForURL)),
     email: Joi.string().email().required(),
     password: Joi.string().min(8).required(),
