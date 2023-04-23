1d633773a75faf9c3c0d76583746b59333a7212c
stanken_a <artyemst3@mail.ru>
2023-04-23 14:29:36 +0300

    fix: fix cards information for getCards controller

--- controllers/cards.js
+++ controllers/cards.js
@@ -5,6 +5,7 @@ const Card = require('../models/card');
 
 const getCards = (req, res, next) => {
   Card.find({})
+    .populate(['owner', 'likes'])
     .then((cards) => res.send(cards))
     .catch(next);
 };
