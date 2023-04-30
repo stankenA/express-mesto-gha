fff555379a1533b1372375f93b8f7e2f2cfb2234
stanken_a <artyemst3@mail.ru>
2023-04-30 20:21:52 +0300

    fix: change ip for mongoose connection

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(express.json()); // для сборки JSON-формата
 app.use(express.urlencoded({ extended: true })); // для приёма веб-страниц внутри POST-запроса
 
-mongoose.connect('mongodb://localhost:27017/mestodb');
+mongoose.connect('mongodb://158.160.50.5:27017/mestodb');
 
 app.use(requestLogger);
 
